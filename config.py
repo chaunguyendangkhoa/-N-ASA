551b7c2b3dcfe52888d198f968849bacb4c40779
chaunguyendangkhoa <khoadangtt13@gmail.com>
2026-07-04 19:27:41 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,15 @@
 import streamlit as st
+import os
+
 APP_NAME = "AI Study Assistant"
-
 VERSION = "1.0"
-
 AUTHOR = "Châu Nguyễn Đăng Khoa"
 
 MODEL_NAME = "llama-3.3-70b-versatile"
 
-GROQ_API_KEY="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"
+GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
 
 DATABASE_NAME = "database/history.db"
 
 EXPORT_FOLDER = "exports"
-
 UPLOAD_FOLDER = "uploads"
